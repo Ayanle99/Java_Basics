ac224cffd4c843f3354363f2ec9d66606d2169d9
haykal <ayanle226@gmail.com>
2019-05-30 18:03:15 -0500

    Added data types to the main java class

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Hello, world");
+
+
+		int age = 29;
+		float decimal = 18.89;
+		double dp = 19.84;
+		long longNumber = 199384994929L;
+		boolean trueOrFalse = false;
+		char character = 'a';
+		String string = "Name";
+		
+
+
+
+
 	}
 }
